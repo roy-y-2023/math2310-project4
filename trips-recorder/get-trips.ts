ee772d79b15d4409aed99d0e1b8a18a290b6a953
roy-y-2023 <153246416+roy-y-2023@users.noreply.github.com>
2024-11-25 21:02:31 -0800

    playing around with the data

--- trips-recorder/get-trips.ts
+++ trips-recorder/get-trips.ts
@@ -4,10 +4,16 @@ import {getOBAClientOptions} from "./oba-client-helper.ts";
 
 
 const ROUTES = [
-    "102745"
+    "1_102745"
 ];
 
 
-const oba = new OnebusawaySDK(getOBAClientOptions());
+const oba: OnebusawaySDK = new OnebusawaySDK(getOBAClientOptions());
 
-console.log(await oba.currentTime.retrieve());
+// console.log(await oba.currentTime.retrieve());
+
+for (let route of ROUTES){
+    console.log(`Getting trips for ${route}`);
+    let details = await oba.tripsForRoute.list(route)
+    console.log(details.data.references.trips);
+}
