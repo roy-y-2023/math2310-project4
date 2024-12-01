c9df15ce7856c27ddf8b774a03303525d15a92da
roy-y-2023 <153246416+roy-y-2023@users.noreply.github.com>
2024-12-01 11:33:06 -0800

    remove anomaly data

--- trips-analysis/Get ScheduleDeviation.sql
+++ trips-analysis/Get ScheduleDeviation.sql
@@ -3,4 +3,5 @@ SELECT
 FROM TripStatus
 WHERE routeID = '40_100479'
 AND collectedAt >= 1732694400000 AND collectedAt < 1732780800000  -- 2024-11-27 to 2024-11-28
-AND distanceAlongTrip > 0; -- ignore trip not started yet
+AND distanceAlongTrip > 0 -- ignore trip not started yet
+AND tripID NOT LIKE '%Dup'
